43ffcc48ec81082f0f07c9ec7f80545a498c9f26
Daniel Boulby <Daniel@dyn1221-172.wlan.ic.ac.uk>
2018-02-15 12:00:12 +0000

    Add comments before functions

--- webenv/smartbox/smartboxWeb/models.py
+++ webenv/smartbox/smartboxWeb/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from datetime import datetime
 
+# MailBox object to store the status of each mailbox
 class MailBox(models.Model):
     serial_id = models.IntegerField(default=0)
     mailcount = models.IntegerField(default=0)
@@ -8,11 +9,12 @@ class MailBox(models.Model):
     def __str__(self):
         return str(self.serial_id)
 
+# Object to store a delivery event
 class DeliveredPost(models.Model):
     delivery_time = models.DateTimeField(default=datetime.now, blank=True)
     mailBox = models.ForeignKey(MailBox, on_delete=models.CASCADE, default=0)
 
-
+# Object to store a collection event
 class MailCollected(models.Model):
     collection_time = models.DateTimeField(default=datetime.now, blank=True)
     mailBox = models.ForeignKey(MailBox, on_delete=models.CASCADE, default=0)
